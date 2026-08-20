c340ccbcfe8f5e5602b577b2fb6fd8a8a5e48f05
sanket-ifinworth <sanket@ifinworth.com>
2026-08-20 18:12:08 +0530

    Add complete Angular application

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    OPENROUTER_URL: 'https://openrouter.ai/api/v1/chat/completions',
+    OPENROUTER_API_KEY: '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',
+    MODEL:'nvidia/nemotron-3-ultra-550b-a55b:free'
+};
